58d6c99854e12296a6eae6b719957f7f2476d50e
America-Miguel-Parra <isc19350307@gmail.com>
2023-03-31 09:28:39 -0600

    Estructurando Shopping_list

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+function App() {
+
+  return (
+    <div className="container text-center">
+      <h1 className="mt-3">Shopping List</h1>
+      <hr/>
+      <div className="row mt-4">
+        <div className="col">C</div>
+        <div className="col-8">NOMBRE ITEM</div>
+        <div className="col-3">BOTONES</div>
+      </div>
+
+      <div className="row mt-4">
+        <div className="col">C</div>
+        <div className="col-8">NOMBRE ITEM</div>
+        <div className="col-3">BOTONES</div>
+      </div>
+
+      <div className="row mt-4">
+        <div className="col">C</div>
+        <div className="col-8">NOMBRE ITEM</div>
+        <div className="col-3">BOTONES</div>
+      </div>
+
+    </div>
+    
+  )
+}
+
+export default App
