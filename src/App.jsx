f43130d349ef2a737369fb3725d044ea1aa19963
America-Miguel-Parra <isc19350307@gmail.com>
2023-04-05 19:11:45 -0600

    Formato responsive para pantalla pequeña, añadiendo botones

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,45 @@ function App() {
       <h1 className="mt-3">Shopping List</h1>
       <hr/>
       <div className="row mt-4">
-        <div className="col">C</div>
-        <div className="col-8">NOMBRE ITEM</div>
-        <div className="col-3">BOTONES</div>
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col">1Kg</div>
+        <div className="col-6 col-md-8">Tortillas</div>
+        <div className="col-5 col-md-3 btn-group" role="group">
+        <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
+        <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
+        <button type="button" class="btn btn-outline-danger bi bi-trash3"></button>
+        </div>
       </div>
 
       <div className="row mt-4">
-        <div className="col">C</div>
-        <div className="col-8">NOMBRE ITEM</div>
-        <div className="col-3">BOTONES</div>
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col">1L</div>
+        <div className="col-6 col-md-8">Aceite</div>
+        <div className="col-5 col-md-3 btn-group" role="group">
+        <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
+        <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
+        <button type="button" class="btn btn-outline-danger bi bi-trash3"></button>
+        </div>
       </div>
 
       <div className="row mt-4">
-        <div className="col">C</div>
-        <div className="col-8">NOMBRE ITEM</div>
-        <div className="col-3">BOTONES</div>
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col">3L</div>
+        <div className="col-6 col-md-8">Refresco</div>
+        <div className="col-5 col-md-3 btn-group" role="group">
+        <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
+        <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
+        <button type="button" class="btn btn-outline-danger bi bi-trash3"></button>
+        </div>
       </div>
+
+      
 
     </div>
     
