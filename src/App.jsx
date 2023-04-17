4d4c2176342148c8562e6d17208633aebff02909
America-Miguel-Parra <isc19350307@gmail.com>
2023-04-17 09:06:56 -0600

    Formato responsive para pantalla pequeña, añadiendo botones, modificando encabezado

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 function App() {
 
   return (
-    <div className="container text-center">
-      <div className="row">
+    <div className="container text-center mt-3">
+      <div className="row mt-5">
         <div className="col text-start">
+          <h1>Shopping List</h1>
+          <hr />
+        </div>
+        <div className="col text-end">
+          <button type="button" className="btn btn-outline-primary bi bi-plus-circle">
+          </button>
         </div>
       </div>
-      <h1 className="mt-3">Shopping List</h1>
-      <hr/>
-      <div className="col text-end">
-        <button type="button" className="btn btn-outline-primary">
-          <i className="bi bi-plus-circle"></i>
-        </button>
-      </div>
-      
+
       <div className="row mt-4">
         <div className="col">
           <input type="checkbox" />
@@ -21,9 +20,9 @@ function App() {
         <div className="col">1Kg</div>
         <div className="col-6 col-md-8">Tortillas</div>
         <div className="col-5 col-md-3 btn-group" role="group">
-        <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
-        <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
-        <button type="button" class="btn btn-outline-danger bi bi-trash3"></button>
+          <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
+          <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
+          <button type="button" class="btn btn-outline-danger bi bi-trash3"></button>
         </div>
       </div>
 
@@ -34,9 +33,9 @@ function App() {
         <div className="col">1L</div>
         <div className="col-6 col-md-8">Aceite</div>
         <div className="col-5 col-md-3 btn-group" role="group">
-        <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
-        <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
-        <button type="button" class="btn btn-outline-danger bi bi-trash3"></button>
+          <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
+          <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
+          <button type="button" class="btn btn-outline-danger bi bi-trash3"></button>
         </div>
       </div>
 
@@ -47,16 +46,16 @@ function App() {
         <div className="col">3L</div>
         <div className="col-6 col-md-8">Refresco</div>
         <div className="col-5 col-md-3 btn-group" role="group">
-        <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
-        <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
-        <button type="button" class="btn btn-outline-danger bi bi-trash3"></button>
+          <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
+          <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
+          <button type="button" class="btn btn-outline-danger bi bi-trash3"></button>
         </div>
       </div>
 
-      
+
 
     </div>
-    
+
   )
 }
 
