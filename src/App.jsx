91352b9fbdd78cd0911d6647919606104b585246
America-Miguel-Parra <isc19350307@gmail.com>
2023-04-18 08:56:21 -0600

    Funcionalidad al checkbox TORTILLAS, cuando se selecciona la casilla se raya el contenido

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,61 @@
+import { useState } from "react"
+
 function App() {
+  const [itemChecked, setItemChecked] = useState({
+    first: false,
+    second: false,
+    third: false,
+  })
+
+const handleItemChecked = (e) => {
+  //if (itemChecked[e.target.name]){
+    //setItemChecked({
+      //...itemChecked,
+      //[e.target.name]:false,
+   // });
+
+   setItemChecked({
+    ...itemChecked,
+    [e.target.name]: !itemChecked[e.target.name]
+   })
+    
+  }
+
 
   return (
     <div className="container text-center mt-3">
       <div className="row mt-5">
         <div className="col text-start">
           <h1>Shopping List</h1>
-          <hr />
         </div>
         <div className="col text-end">
           <button type="button" className="btn btn-outline-primary bi bi-plus-circle">
           </button>
         </div>
+        <hr />
       </div>
 
       <div className="row mt-4">
         <div className="col">
-          <input type="checkbox" />
+          <input 
+          checked={itemChecked.first}
+          name="first"
+          onClick={(e)=>handleItemChecked(e)}
+          type="checkbox"/>
         </div>
-        <div className="col">1Kg</div>
-        <div className="col-6 col-md-8">Tortillas</div>
+        <div className="col">
+          {
+            //itemChecked.first && <del>1 kg</del>
+
+          }
+          {
+            //itemChecked.first && "1 kg"
+          }
+          {
+          itemChecked.first ? <del>1Kg</del>: "1 kg" 
+          }
+          </div>
+        <div className="col-6 col-md-8" style={{textDecoration: itemChecked.first && "line-through"}}>Tortillas</div>
         <div className="col-5 col-md-3 btn-group" role="group">
           <button type="button" class="btn btn-outline-primary bi bi-pencil-square"></button>
           <button type="button" class="btn btn-outline-secondary bi bi-files"></button>
